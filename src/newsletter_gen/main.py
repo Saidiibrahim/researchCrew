d8be88327b6df00b041167f9b718b884cff81d25
ibrahimsaidi <ibrahim.aka.ajax@gmail.com>
2024-05-29 12:52:46 +0930

    Adding `.env.example` file and poetry entry point 🚀

--- src/newsletter_gen/main.py
+++ src/newsletter_gen/main.py
@@ -2,9 +2,14 @@
 from newsletter_gen.crew import NewsletterGenCrew
 
 
-def run():
+def main():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
         'topic': 'AI LLMs'
     }
     NewsletterGenCrew().crew().kickoff(inputs=inputs)
+
+
+if __name__ == '__main__':
+    main()
+
